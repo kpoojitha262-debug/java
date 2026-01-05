7d3e2973e441b51700c4f70feb24763703259ca6
Kedarisetti Poojitha <kpoojitha262@gmail.com>
2026-01-05 19:26:06 +0300

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String args[]) {
+        int i = 16;
+       if(num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
