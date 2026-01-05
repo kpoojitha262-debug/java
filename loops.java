d24099a1f7d10571ff73dd8a54ba0e9e62007a67
Kedarisetti Poojitha <kpoojitha262@gmail.com>
2026-01-05 19:30:31 +0300

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,28 @@
+//For loop
+class ForLoop {
+    public static void main(String args[]) {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+//While 
+class While {
+ public static void main(String[] args) {
+   int i = 1;
+        while(i <= 5) {
+            System.out.println(i);
+            i++;
+     }
+   }
+}
+//Do-While
+class DoWhile{
+public static void main (String[] args){
+int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 5);
+    }
+}
