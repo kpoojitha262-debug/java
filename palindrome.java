c94c55f37021f60e4a377b9ef0b37c6e2e6f1a5d
Kedarisetti Poojitha <kpoojitha262@gmail.com>
2026-01-06 19:42:36 +0300

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,13 @@
+int num = 121;
+int original = num;
+int rev = 0;
+
+while (num != 0) {
+    rev = rev * 10 + num % 10;
+    num /= 10;
+}
+
+if (original == rev)
+    System.out.println("Palindrome");
+else
+    System.out.println("Not Palindrome");
